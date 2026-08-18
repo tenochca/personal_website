80343b183967523ef950f17aa4d7796416fc6c3b
tenochca <tenochca>
2026-08-17 21:35:26 -0500

    rescructured project to work with jinja templates

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
