14fba774d7b211f0386b13d0f76e60d52e9630d9
tenochca <tenochca>
2026-08-20 23:27:14 -0500

    readded the scripts and also used fastAPI to serve html and static files via jinja templates

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request, "index.html")
+
+@app.get("/about", response_class=HTMLResponse)
+def about(request: Request):
+    return templates.TemplateResponse(request, "about.html")
+
+@app.get("/projects", response_class=HTMLResponse)
+def projects(request: Request):
+    return templates.TemplateResponse(request, 'projects.html')
+
+@app.get("/logs", response_class=HTMLResponse)
+def logs(request: Request):
+    return templates.TemplateResponse(request, 'logs.html')
