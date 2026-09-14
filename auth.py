cf45205af4f8cdeca4e70e05a46cf9262886a681
tenochca <tenochca.di@outlook.com>
2026-09-13 22:01:32 -0500

    Auth will authentics admin using Basic HTTP credentials

--- auth.py
+++ auth.py
@@ -8,3 +8,15 @@ security = HTTPBasic()
 
 ADMIN_USERNAME = os.environ["ADMIN_USERNAME"]
 ADMIN_PASSWORD = os.environ["ADMIN_PASSWORD"]
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, ADMIN_USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, ADMIN_PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException (
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
